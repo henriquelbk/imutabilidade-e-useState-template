1ef0663614e4fad43d5f10037f98055610fe5492
henriquelbk <loubackhenrique123@gmail.com>
2023-05-23 19:13:57 -0300

    template

--- src/Componentes/Garagem/index.js
+++ src/Componentes/Garagem/index.js
@@ -1,19 +1,32 @@
 import { Carro } from '../Carro'
 import { Botao, Estacionamento, GaragemContainer } from './styles'
 
-export function Garagem({ nome }) {
+export function Garagem({ props }) {
+
+const{nomeGaragem, setNome, automovel, automovel1, setAutomovel}= props
+
+const{modelo, ano, cor, adicionadoPor, flex}= automovel
+
+function alterarNome(){
+
+  setNome("Easley")
+
+}
+
   return (
     <GaragemContainer>
-      <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
+      <h1>Garagem da {nomeGaragem}</h1>
+      <Botao onClick={alterarNome}>Muda nome</Botao>
 
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          modelo={modelo}
+          cor={cor}
+          ano={ano}
+          adicionadoPor={adicionadoPor}
+          flex={flex}
+          setAutomovel={setAutomovel}
+          automovel1={fusca}
         />
       </Estacionamento>
     </GaragemContainer>
