28e24bbdd330d6206a91efe2bb73875e898c3bf7
henriquelbk <loubackhenrique123@gmail.com>
2023-05-23 19:14:08 -0300

    template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import { Garagem } from './Componentes/Garagem'
 import { GlobalStyled } from './GlobalStyled'
+import { useState } from 'react'
 
 export default function App() {
+
+  const [nome, setNome] = useState('Labenu')
+  const [automovel, setAutomovel] = useState({
+    modelo: "corsa",
+    cor: "azul",
+    ano: 2005,
+    adicionadoPor: nome,
+    flex: true,
+  })
+
+  const fusca = {
+    modelo: "fusca",
+    cor: "azul",
+    ano: 1975,
+    adicionadoPor: nome,
+    flex: true,
+  }
+
+  const gol = {
+    modelo: "gol",
+    cor: "prata",
+    ano: 2009,
+    adicionadoPor: nome,
+    flex: false,
+  }
+
+  const parati = {
+    modelo: "parati",
+    cor: "vinho",
+    ano: 1984,
+    adicionadoPor: nome,
+    flex: false,
+  }
+
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem nomeGaragem={nome} 
+      setNome = {setNome}
+      automovel = {automovel}
+      setAutomovel={setAutomovel}
+      automovel1={fusca} />
     </div>
   )
 }
--- src/Componentes/Carro/index.js
+++ src/Componentes/Carro/index.js
@@ -1,6 +1,11 @@
 import { CarroContainer } from './styles'
 
-export function Carro({ modelo, cor, ano, flex, adicionadoPor }) {
+export function Carro({ modelo, cor, ano, flex, adicionadoPor,automovel1, setAutomovel}) {
+
+  function alterarCarro() {
+    setAutomovel(automovel1)
+  }
+
   return (
     <CarroContainer>
       <h2>{modelo}</h2>
@@ -10,6 +15,7 @@ export function Carro({ modelo, cor, ano, flex, adicionadoPor }) {
         <li>Flex: {flex ? 'Sim' : 'Não'}</li>
         <li>Adicionado: {adicionadoPor}</li>
       </ul>
+      <button onClick={alterarCarro}>Mudar Carro</button>
     </CarroContainer>
   )
 }
